dc50da18c02e63d9d1961374d5c8dd7de9c8b530
Can Dincer <dincercan@gmail.com>
2017-12-26 14:23:47 +0300

    Initial commit

---
+++ app/src/main/java/com/vogella/android/videoviewcard/MainActivity.java
@@ -0,0 +1,34 @@
+package com.vogella.android.videoviewcard;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final VideoView videoView =
+                (VideoView) findViewById(R.id.simpleVideoView);
+        VideoView video=(VideoView) findViewById(R.id.simpleVideoView);
+
+        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.fishvideo);
+
+
+        videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+    }
+
+
+
+
+
+}
